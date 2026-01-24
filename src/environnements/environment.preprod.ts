1126b458e6524407562447c8ef11d6efcc51336e
SamEva-dev <sam.fokam@gmail.com>
2026-01-25 00:05:30 +0100

    fixe env

--- src/environnements/environment.preprod.ts
+++ src/environnements/environment.preprod.ts
@@ -1,5 +1,8 @@
 export const environment = {
     BASE_AUTH_API: "https://auth.preprod.locaguest.com",
     BASE_LOCAGUEST_API: "https://api.preprod.locaguest.com",
+    CHATBOT_AI_BASE_URL: '',
+    CHATBOT_AI_MODEL: '',
+    CHATBOT_AI_ENABLED: false,
     production: true
   };
--- src/environnements/environment.staging.ts
+++ src/environnements/environment.staging.ts
@@ -1,5 +1,8 @@
 export const environment = {
     BASE_AUTH_API: "https://auth.staging.locaguest.com",
     BASE_LOCAGUEST_API: "https://api.staging.locaguest.com",
+    CHATBOT_AI_BASE_URL: '',
+    CHATBOT_AI_MODEL: '',
+    CHATBOT_AI_ENABLED: false,
     production: false
   };
